d61f925d1c157b76c9a3a3b128dc54c12294a457
Anand6262 <pyexpert62@gmail.com>
2025-03-18 11:19:05 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -14,9 +14,14 @@ function App() {
 function Counter() {
   const [count, setCount] = useState(0)
   return (
-    <div>
-      <p>{count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+    <div className="bg-white shadow-lg rounded-lg p-6 w-full sm:w-50 md:w-80 text-center mx-4 my-4 max-w-full">
+      <p className="text-2xl font-bold text-blue-600 mb-4">{count}</p>
+      <button
+        onClick={() => setCount(count + 1)}
+        className="bg-blue-500 text-white p-2 rounded-full hover:bg-blue-600 transition duration-300"
+      >
+        Increment
+      </button>
     </div>
   )
 }
--- src/commonComponents/Hoc.jsx
+++ src/commonComponents/Hoc.jsx
@@ -5,8 +5,21 @@ function Hoc(props) {
 
   return (
     <div>
-      <h1>Hoc</h1>
-      <p>{props.com()}</p>
+      <h1 className="text-center font-bold text-2xl mb-4">Hoc</h1>
+      <div className="grid grid-cols-4 gap-4">
+        <p className="bg-red-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-blue-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-green-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-yellow-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-purple-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-pink-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-indigo-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-teal-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-orange-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-lime-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-gray-300 p-4 rounded">{props.com()}</p>
+        <p className="bg-emerald-300 p-4 rounded">{props.com()}</p>
+      </div>
     </div>
   );
 }
