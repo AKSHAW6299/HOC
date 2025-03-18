99dc873c7f2ff8c54099677cd499fc5649a86d3f
Anand6262 <pyexpert62@gmail.com>
2025-03-18 11:05:49 +0530

    chanegs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Hoc from './commonComponents/Hoc'
+
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      {/* We are passing the Counter component as an argument to Hoc! */}
+      <Hoc com={Counter} />
+    </div>
   )
 }
+
+function Counter() {
+  const [count, setCount] = useState(0)
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  )
+}
+
 
 export default App
--- src/commonComponents/Hoc.jsx
+++ src/commonComponents/Hoc.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
-function Hoc() {
+function Hoc(props) {
+  console.log("props :", props);
+
   return (
-    <div>Hoc</div>
-  )
+    <div>
+      <h1>Hoc</h1>
+      <p>{props.com()}</p>
+    </div>
+  );
 }
 
-export default Hoc
+export default Hoc;
